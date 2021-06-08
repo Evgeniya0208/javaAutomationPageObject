dc642142dcf7116ba09af05d4744eb062eda6266
evgeniyatyutyunik <evgeniya.tyutyunik@gmail.com>
2021-06-08 09:02:01 +0300

    page object

--- src/test/java/com/customertimes/test/login/LoginSuccessTest.java
+++ src/test/java/com/customertimes/test/login/LoginSuccessTest.java
@@ -22,14 +22,11 @@ public class LoginSuccessTest extends BaseTest {
 
     @BeforeClass
     public void setup() throws InterruptedException {
-        wait = new WebDriverWait(getWebDriver(), 5);
-        getWebDriver().get("http://beeb0b73705f.sn.mynetname.net:3000/#/login");
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[aria-label = 'Close Welcome Banner']")));
-        WebElement dismissButton = getWebDriver().findElement(By.cssSelector("[aria-label = 'Close Welcome Banner']"));
-        dismissButton.click();
-
         customer = Customer.newBuilder().withName("evgeniya1@gmail.com").withPassword("123456").build();
         loginPage = new LoginPage(driver);
+        loginPage.openPage();
+
+        customer = Customer.newBuilder().withName("evgeniya1@gmail.com").withPassword("123456").build();
     }
 
     @AfterClass
--- src/test/java/com/customertimes/test/pages/LoginPage.java
+++ src/test/java/com/customertimes/test/pages/LoginPage.java
@@ -30,7 +30,11 @@ public class LoginPage extends AbstractPage {
 
     @Override
     public void openPage() {
-        driver.get(loginPage);
+        wait = new WebDriverWait(getWebDriver(), 5);
+        getWebDriver().get("http://beeb0b73705f.sn.mynetname.net:3000/#/login");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[aria-label = 'Close Welcome Banner']")));
+        WebElement dismissButton = getWebDriver().findElement(By.cssSelector("[aria-label = 'Close Welcome Banner']"));
+        dismissButton.click();
     }
 
     public String getActualUserName(String currentEmail) {
